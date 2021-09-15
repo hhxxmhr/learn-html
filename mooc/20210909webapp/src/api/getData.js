4721d9c4093a6e418d9b35cfa34711aad176516f
咕噜咕噜hi仝 <1848554565@qq.com>
2021-09-15 15:01:51 +0800

    fix：改写请求函数，让返回值携带xhr属性

--- mooc/20210909webapp/src/api/getData.js
+++ mooc/20210909webapp/src/api/getData.js
@@ -1,29 +1,32 @@
-import { SUCC_CODE, TIMEOUT } from 'api/config';
-import { getJSON } from './ajax';
+import {SUCC_CODE, TIMEOUT} from 'api/config';
+import {getJSON} from './ajax';
 
 // 获取数据
 const getData = (url, options) => {
-  return getJSON(url, { timeoutTime: TIMEOUT, ...options })
-    .then(response => {
-      if (response.code !== SUCC_CODE) throw new Error('出错了');
+    const ajaxPromise = getJSON(url, {timeoutTime: TIMEOUT, ...options});
+    const resultPromise = ajaxPromise.then(response => {
+        if (response.code !== SUCC_CODE) throw new Error('出错了');
 
-      return response.data;
+        return response.data;
     })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
+    resultPromise.xhr = ajaxPromise.xhr;
+    return resultPromise;
+
 };
 
 // 延时
 const delay = ms => {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
 };
 
 // 获取延迟的数据
 const getDelayedData = (url, options) => {
-  return delay(1000).then(() => {
-    return getData(url, options);
-  });
+    return delay(1000).then(() => {
+        return getData(url, options);
+    });
 };
 
-export { getData, getDelayedData };
+export {getData, getDelayedData};
